f7abb4d10156bd603dc2c02e7dc2d9dc58274b8f
markkramerus <mkramer@mitre.org>
2020-03-24 22:17:25 -0400

    remove old icare

--- docs/icare/RequestIntent.java
+++
@@ -1,7 +0,0 @@
-package http://mcodeinitiative.org/codex/us/icare/ImplementationGuide/mcodeinitiative.icare-0.1.2;
-
-import org.hl7.fhir.r5.model.ProfilingWrapper;
-
-public class RequestIntent {
-
-}
